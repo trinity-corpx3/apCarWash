accaac31d7bcd9b6da441e07a4c9e81b3c487595
dario.mata <dario.mata@192.168.1.65>
2026-03-14 08:49:50 -0600

    fix: CORS config to allow all origin patterns via proxy

--- backend/src/main/java/com/trinity/poserp/security/CorsConfig.java
+++ backend/src/main/java/com/trinity/poserp/security/CorsConfig.java
@@ -14,13 +14,7 @@ public class CorsConfig {
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of(
-                "http://localhost:4200",
-                "http://poserprl.s3-website.us-east-2.amazonaws.com",
-                "https://rlautolavado.com",
-                "https://www.rlautolavado.com",
-                "https://apcarwash.trinitycorp.mx",
-                "https://www.apcarwash.trinitycorp.mx"));
+        config.setAllowedOriginPatterns(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type", "X-Requested-With", "Accept", "Origin"));
 
